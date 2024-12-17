fb94ff2fd92b604134c980d248afca4fcb8881f9
Debianov <regdeb@mail.ru>
2024-12-17 17:55:58 +0800

    Добавлен тест к main.
    Доделан на 1/3

--- main_test.go
+++ main_test.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestErrorHandler(t *testing.T) {
+	var (
+		w = httptest.NewRecorder()
+		expectedErrResponse = &ErrorResponse{Error: "Expression is not valid"}
+		currentErrResponse *ErrorResponse
+		buf *bytes.Buffer
+		err error
+	)
+	expressionValidErrorHandler(w)
+	if w.Code != 422 {
+		t.Errorf("ожидается код 422, получен %d", w.Code)
+	}
+	buf = w.Body
+	, err = json.Unmarshal(, currentErrResponse)
+	if currentErrResponse.Error != expectedErrResponse.Error {
+
+	}
+
+}
