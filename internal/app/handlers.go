3e1d4a1de057779d61c59215a2932be7eb9c875c
debianov <regdeb@mail.ru>
2024-12-19 14:47:01 +0800

    Формирование handler перенесено.

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -92,3 +92,10 @@ func internalServerErrorHandler(w http.ResponseWriter) {
 	}
 	return
 }
+
+func getHandler() (handler http.Handler) {
+	var mux = http.NewServeMux()
+	mux.HandleFunc("/api/v1/calculate", CalcHandler)
+	handler = PanicMiddleware(mux)
+	return
+}
--- internal/app/server.go
+++ internal/app/server.go
@@ -2,14 +2,10 @@ package app
 
 import (
 	"github.com/Debianov/calc-ya-go-24/internal"
-	"net/http"
 )
 
 func StartServer() (err error) {
-	var mux = http.NewServeMux()
-	mux.HandleFunc("/api/v1/calculate", CalcHandler)
-	var handler = PanicMiddleware(mux)
-	s := internal.GetDefaultServer(handler)
+	s := internal.GetDefaultServer(getHandler())
 	err = s.ListenAndServe()
 	return
 }
