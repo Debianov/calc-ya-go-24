8bd87d1797fdd4745742a28d73d36a7a7d9ed5eb
Debianov <regdeb@mail.ru>
2025-03-02 17:17:29 +0800

    Добавлены переменные окружения.
    Необходимы для соответствия тест-кейсам.
    +рефакторинг

--- backend/agent/server.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/Debianov/calc-ya-go-24/backend/orchestrator"
-
-func StartServer() (err error) {
-	s := GetDefaultServer(orchestrator.getHandler())
-	err = s.ListenAndServe()
-	return
-}
